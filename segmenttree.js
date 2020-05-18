deb8bc2ab8ed6fae49c21a455835e37e541ad97c
sagarmanghnani <sagarmanghnani@gmail.com>
2020-05-19 01:47:14 +0530

    fixed updation code for segment code

--- segmenttree.js
+++ segmenttree.js
@@ -1,117 +1,88 @@
-class SegmentTree {
-  
-    constructor(operator){
-      this.storeSegmentTree = [];
+class SegmentTree{
+    
+  constructor(operator, iterable){
       this.sentinal = new Object();
-      this.operate = ((arg1,arg2) => {
-        if(arg1 == this.sentinal){
-          return arg2;
-        }
-        
-        else if(arg2 == this.sentinal){
-          return arg1;
-        }
-        else{
-          return operator(arg1, arg2);
-        }
-        
+      this.arrToProcess = [];
+      this.segTreeArr = [];
+      for(let iter of iterable){
+          this.arrToProcess.push(iter);
+      }
+      
+      this.operate = ((a, b) => {
+         if(a == this.sentinal){
+             return b;
+         }else if(b == this.sentinal){
+             return a;
+         }else{
+             return operator(a, b);
+         } 
       });
-    }
-    
-    set segmentDataToAssign(data){
-      this.segDataToProcess = [];
-      for(let element of data){
-        this.segDataToProcess.push(element);
-      }
-    }
+      
+      this.createSegTree(0, this.arrToProcess.length - 1, 0);
+  }
   
-    get segmentDataToAssign(){
-      
-      return this.storeSegmentTree;
-    }
-    
-    
-    buildSegTree(left , right, pos){
-      
-      if(left == right){
-        this.storeSegmentTree[pos] = this.segDataToProcess[left];
-        return this.storeSegmentTree[pos];
+  createSegTree(low, high, pos){
+      if(low == high){
+          this.segTreeArr[pos] = this.arrToProcess[low];
+          return this.segTreeArr[pos];
       }else{
-        var mid = Math.floor((left + right) / 2);
-        var left_segtree = this.buildSegTree(left, mid, 2*pos + 1);
-        var right_segtree = this.buildSegTree(mid + 1, right, 2*pos + 2);
-        
-        this.storeSegmentTree[pos] = this.operate(left_segtree, right_segtree);
-        return this.storeSegmentTree[pos];
-      }
-      
-    }
-    
-    _searchRange(q1, q2, r1, r2, pos){
-      if(q2 < r1 || r2 < q1){
-        return this.sentinal;
-      }else if(r1 >= q1 && r2 <= q2){
-       
-        return this.storeSegmentTree[pos];
-      }else{
-        
-        var mid = Math.floor((r1+r2)/2);
-        
-        var left_sum = this._searchRange(q1, q2, r1, mid, 2*pos + 1);
-        var right_sum = this._searchRange(q1, q2, mid + 1, r2 , 2*pos + 2);
-        
-        return this.operate(left_sum, right_sum);
-      }
-    }
-    
-    updateSegtree(index, r1, r2, diff, pos){
-      
-      if(r1 == r2 && r1 == index){
-       
-        this.storeSegmentTree[pos] = this.operate(this.storeSegmentTree[pos], diff);
-        return;
-      }else{
-        var mid = Math.floor((r1 + r2) / 2);
-        if(mid >= index){
-          this.updateSegtree(index, r1, mid, diff, 2*pos + 1);
+          var mid = (low + high) >>> 1;
+          var left_sum = this.createSegTree(low, mid, 2*pos + 1);
+          var right_sum = this.createSegTree(mid + 1, high, 2*pos + 2);
           
-          this.storeSegmentTree[pos] = this.operate(this.storeSegmentTree[pos], diff);
-        }else{
-          this.updateSegtree(index, mid + 1, r2, diff, 2*pos + 2);
-          this.storeSegmentTree[pos] = this.operate(this.storeSegmentTree[pos], diff);
-        }
+          this.segTreeArr[pos] = this.operate(left_sum, right_sum);
+          return this.segTreeArr[pos];
       }
-    }
-    
-    updateQueryByUser(index, updateValue){
-      var diff = updateValue - this.segDataToProcess[index];
-      this.updateSegtree(index, 0, this.segDataToProcess.length, diff,0);
-    }
-    
-    
-    searchQueryByUser(q1, q2){
-      if(q2 > this.segDataToProcess.length - 1 || q1 < 0){
-        return -1;
+  }
+  
+  _searchQuery(r1, r2, q1, q2, pos){
+      
+      if(r1 > q2 || r2 < q1){
+          return this.sentinal;
+      }else if(q1 <= r1 && q2 >= r2){
+          return this.segTreeArr[pos]
       }else{
-        return this._searchRange(q1,q2, 0, this.segDataToProcess.length - 1, 0)
+          
+          var mid = (r1 + r2) >>> 1;
+          
+          var left_sum = this._searchQuery(r1, mid, q1, q2, 2*pos + 1);
+          var right_sum = this._searchQuery(mid + 1, r2, q1, q2, 2*pos + 2);
+          
+          
+          
+          
+          return this.operate(left_sum, right_sum);
       }
-    }
-    
   }
   
-  function add(num1, num2){
-    return num1 + num2;
+  searchQueryForUser(q1, q2){
+      var result =  this._searchQuery(0, this.arrToProcess.length - 1, q1, q2, 0);
+      
+      return result;
   }
-  var temp = [1,2,3,4,5,6,7];
-  var segTree = new SegmentTree(add);
-  segTree.segmentDataToAssign = temp;
-  segTree.buildSegTree(0, temp.length - 1, 0);
   
-  var result = segTree.searchQueryByUser(2, 2);
-  segTree.updateQueryByUser(4, 8);
+  _updateSegTree(index, updateValue, low, high, pos){
+      if(low == high){
+          if(low == index){
+              this.segTreeArr[pos] = updateValue;
+          }
+          return this.segTreeArr[pos];
+      }else if(low > index || index > high){
+          //this means it is out of place
+          return this.segTreeArr[pos];
+      }else{
+          
+          var mid = (low + high) >>> 1;
+          var left_sum = this._updateSegTree(index, updateValue, low, mid, 2*pos + 1);
+          var right_sum = this._updateSegTree(index, updateValue, mid + 1, high, 2*pos + 2);
+          this.segTreeArr[pos] = this.operate(left_sum, right_sum);
+          return this.segTreeArr[pos];
+      }
+  }
   
-  var data = segTree.segmentDataToAssign;
+  updateSegTree(index, updateValue){
+      this._updateSegTree(index, updateValue, 0, this.arrToProcess.length - 1, 0);
+  }
   
   
-  
-  
+}
